9c7691ed5394ee635014d88307188b8f4ad40c49
JeanPoolDev <jeanpoolrojasavila456@gmail.com>
2024-05-14 22:05:43 -0500

    view-description-products

---
+++ components/gallery/index.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Image from "next/image";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import GalleryTab from "./gallery-tab";
+
+import { Image as ImageType } from "@/types";
+
+interface GalleryProps{
+  images: ImageType[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({
+  images
+}) => {
+  return ( 
+    <TabGroup as="div" className="flex flex-col-reverse">
+      <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+        <TabList className="grid grid-cols-4 gap-6">
+          {
+            images.map((image) => (
+              <GalleryTab key={image.id} image={image} />
+            ))
+          }
+        </TabList>
+      </div>
+      <TabPanels className="aspect-square w-full">
+        {
+          images.map((image) => (
+            <TabPanel key={image.id}>
+              <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+                <Image 
+                  fill
+                  src={image.url}
+                  alt="image"
+                  className="object-cover object-center"             
+                />
+              </div>
+            </TabPanel>
+          ))
+        }
+      </TabPanels>
+    </TabGroup>
+   );
+}
+ 
+export default Gallery;
--- components/ui/product-cart.tsx
+++ components/ui/product-cart.tsx
@@ -6,6 +6,7 @@ import { Expand, ShoppingCart } from "lucide-react";
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductoCardProps{
   data: Product;
@@ -14,8 +15,15 @@ interface ProductoCardProps{
 const ProductCard: React.FC<ProductoCardProps> = ({
   data
 }) => {
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  }
+
   return ( 
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Imagenes y Acciones */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image 
